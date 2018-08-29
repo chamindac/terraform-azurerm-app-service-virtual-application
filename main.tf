708265c0efcc30908fc66590f13a83952b19f9ef
Unknown <kevin@rustycog.com>
2018-08-29 10:17:39 -0700

    refactored copy element in template
    application_names: empty array will not fail.

--- main.tf
+++ main.tf
@@ -5,56 +5,51 @@ resource "azurerm_template_deployment" "service_app_virtual_application" {
 
   template_body = <<DEPLOY
 {
-  "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
-  "contentVersion": "1.0.0.0",
-  "parameters": {
-    "appServiceName": {
-      "type": "string"
+    "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
+    "contentVersion": "1.0.0.0",
+    "parameters": {
+        "appServiceName": {
+            "type": "String"
+        },
+        "applicationNames": {
+            "type": "String"
+        }
     },
-    "applicationNames":{
-      "type": "string"
-    }
-  },
-  "variables": {
-      "directoryNames" :"[split(parameters('applicationNames'),',')]",
-       "child": {
+    "variables": {
+        "appNames": "[union(variables('wwwroot'),split(parameters('applicationNames'),','))]",
+        "virtual": {
             "copy": [
                 {
-                    "name": "dir",
-                    "count": "[length(variables('directoryNames'))]",
+                    "count": "[length(variables('appNames'))]",
                     "input": {
-                        "virtualPath": "[concat('/', variables('directoryNames')[copyIndex('dir')]) ]",
-                        "physicalPath": "[concat('site\\wwwroot\\', variables('directoryNames')[copyIndex('dir')]) ]",
+                        "physicalPath": "[concat('site\\wwwroot\\', variables('appNames')[copyIndex('apps')]) ]",
                         "preloadEnabled": false,
-                        "virtualDirectories": null
-                    }
-
+                        "virtualDirectories": null,
+                        "virtualPath": "[concat('/', variables('appNames')[copyIndex('apps')]) ]"
+                    },
+                    "name": "apps"
                 }
             ]
         },
-        "root":[
-            {
-                "virtualPath": "/",
-                "physicalPath": "site\\wwwroot",
-                "preloadEnabled": false,
-                "virtualDirectories": null
-            }
-        ],
-        "virtualDirectories":"[union(variables('child').dir, variables('root'))]"
-  },
-  "resources": [
-    {
-      "comments": "WebApp VirtualDirectories",
-      "condition": "[not(empty(variables('directoryNames')))]",
-      "type": "Microsoft.Web/sites/config",
-      "name": "[concat(parameters('appServiceName'), '/web')]",
-      "apiVersion": "2016-08-01",
-      "properties": {
-        "virtualApplications": "[variables('virtualDirectories')]"
-        },
-      "dependsOn": []
+        "wwwroot": "[split('',',')]"
+    },
+    "resources": [
+        {
+            "type": "Microsoft.Web/sites/config",
+            "name": "[concat(parameters('appServiceName'), '/web')]",
+            "apiVersion": "2016-08-01",
+            "properties": {
+                "virtualApplications": "[variables('virtual').apps]"
+            },
+            "dependsOn": []
+        }
+    ],
+    "outputs": {
+        "virtualApplications": {
+            "type": "Array",
+            "value": "[variables('virtual').apps]"
+        }
     }
-  ]
 }
 DEPLOY
 
